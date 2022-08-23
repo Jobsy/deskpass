c2933c8646023afde32beacc9d45655e3396b012
Oluwajoba Bello <oluwajobabello@gmail.com>
2022-08-24 00:08:50 +0100

    commit changes

--- server/server.ts
+++ server/server.ts
@@ -5,10 +5,6 @@ dotenv.config();
 
 const port = process.env.HTTPPORT || 8000;
 
-
-// documentation
-// dockers
-
 if (process.env.NODE_ENV !== 'test') {
   app.listen(8000, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
